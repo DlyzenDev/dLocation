cd299d4c8c61e593e17be27d80d38996143c1d91
DlyzenDev <136992120+DlyzenDev@users.noreply.github.com>
2024-07-29 09:18:59 +0200

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,9 @@
+Location = {
+    {
+        point = vector3(-1034.531860, -2732.795654, 19.172062),
+        text = "Appuyer sur ~INPUT_CONTEXT~ pour louer un véhicule.",
+        listVehicle = {
+            {label = "Panto", name = "panto", price = 500, spawnpoint = vector4(-1029.7257080078, -2729.6374511719, 20.176523208618, 239.00907897949)}
+        }
+    },
+}
---
+++ fxmanifest.lua
@@ -0,0 +1,24 @@
+fx_version 'adamant'
+game 'gta5'
+
+author 'Dlyzeennn'
+
+client_scripts {
+    "src/RMenu.lua",
+    "src/menu/RageUI.lua",
+    "src/menu/Menu.lua",
+    "src/menu/MenuController.lua",
+    "src/components/*.lua",
+    "src/menu/elements/*.lua",
+    "src/menu/items/*.lua",
+    "src/menu/panels/*.lua",
+    "src/menu/windows/*.lua",
+
+    -- Script
+    "client/*.lua",
+    "config.lua"
+}
+
+server_scripts {
+    "server/*.lua"
+}
